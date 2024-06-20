434b608e4c7489a90a8fc48cff2fe2758d2b3bb7
Nirajadh <100203599+Nirajadh@users.noreply.github.com>
2024-06-21 01:09:41 +0545

    db

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -22,11 +22,39 @@ CREATE TABLE UserUploads (
     ImageData VARBINARY(MAX),
     Caption NVARCHAR(255) null,
     UploadDate DATETIME,
-    CONSTRAINT FK_UserUploads_Users FOREIGN KEY (UserID) REFERENCES Users(UserID) on delete cascade
+    CONSTRAINT FK_UserUploads_Users FOREIGN KEY (UserID) REFERENCES Users(UserID) on delete cascade;
 
 );
 select *from useruploads
-truncate table users
+truncate table users 
 alter table UserUploads add constraint PFK_UserUploads_Users FOREIGN KEY (UserID) REFERENCES Users(UserID) on delete cascade;
-alter table UserUploads drop constraint FK_UserUploads_Users ;
-ALTER TABLE UserUploads ADD Likes INT DEFAULT 0;
+alter table UserUploads drop constraint PFK_UserUploads_Users ;
+ALTER TABLE UserUploads ADD Likes INT DEFAULT 0 not null;
+alter table useruploads
+drop  column likes ;
+
+CREATE TABLE Liked (
+
+UserID INT NOT NULL,
+ UploadID INT not null,
+
+  PRIMARY KEY (UserID, UploadID)
+);
+drop table liked
+
+CREATE TABLE Comments (
+    CommentID INT PRIMARY KEY IDENTITY,
+    UploadID INT FOREIGN KEY REFERENCES UserUploads(UploadID),
+    UserID INT FOREIGN KEY REFERENCES Users(UserID),
+    CommentText NVARCHAR(MAX),
+    CommentDate DATETIME DEFAULT GETDATE() 
+);
+
+
+	DELETE FROM Liked WHERE UserID=3 AND UploadID=5
+
+	DELETE FROM Liked WHERE UploadID= 5
+	select * from Liked
+
+
+	SELECT UploadID, ImageData, Caption, UploadDate, (SELECT Username FROM Users WHERE UserID = UserUploads.UserID)  AS Username FROM UserUploads where Username <> 'Nirajadh'  ORDER BY UploadID DESC
